24bf25cc77f4b31804e3960c2776004756d40fb8
seasameoil <whdwlgus12@gmail.com>
2024-04-07 01:43:13 +0900

    first commit

---
+++ src/main/java/com/zupzup/untact/SpringModuleAuthApplication.java
@@ -0,0 +1,13 @@
+package com.zupzup.untact;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringModuleAuthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringModuleAuthApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zupzup/untact/SpringModuleAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zupzup.untact;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringModuleAuthApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
