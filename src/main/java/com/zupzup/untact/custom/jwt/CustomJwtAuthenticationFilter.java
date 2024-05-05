32caf8d659076e85ac44daa275d3051a12811b86
seasameoil <whdwlgus12@gmail.com>
2024-05-06 00:23:09 +0900

    refactor : auth 분리 진행중 (2)

--- src/main/java/com/zupzup/untact/custom/jwt/CustomJwtAuthenticationFilter.java
+++ src/main/java/com/zupzup/untact/custom/jwt/CustomJwtAuthenticationFilter.java
@@ -1,10 +1,54 @@
 package com.zupzup.untact.custom.jwt;
 
-import com.zupzup.untact.auth.jwt.JwtTokenProvider;
+import com.zupzup.untact.exception.exception.auth.BlackListTokenException;
+import com.zupzup.untact.exception.exception.auth.RefreshRequiredException;
+import com.zupzup.untact.exception.exception.auth.RequiredHeaderNotExistException;
+import com.zupzup.untact.exception.exception.auth.SignFailedException;
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.MalformedJwtException;
+import io.jsonwebtoken.security.SignatureException;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.filter.OncePerRequestFilter;
 
-public class CustomJwtAuthenticationFilter extends com.zupzup.untact.auth.jwt.JwtAuthenticationFilter {
+import java.io.IOException;
 
-    public CustomJwtAuthenticationFilter(JwtTokenProvider jwtTokenProvider) {
-        super(jwtTokenProvider);
+@RequiredArgsConstructor
+public class CustomJwtAuthenticationFilter extends OncePerRequestFilter {
+
+    private final CustomJwtTokenProvider jwtTokenProvider;
+
+    // JwtAuthenticationFilter를 filterChain에 등록
+    @Override
+    public void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws IOException, ServletException {
+        String accessToken = jwtTokenProvider.resolveToken(request, jwtTokenProvider.ACCESS_TOKEN_NAME);
+        if (accessToken == null) throw new RequiredHeaderNotExistException(jwtTokenProvider.ACCESS_TOKEN_NAME);
+
+        if (!jwtTokenProvider.isRedisBlackList(accessToken)) {   // 로그아웃 된 상황이 아니라면(redis refreshToken 테이블에 accessToken이 저장된 게 아니라면)
+            try {
+                if (jwtTokenProvider.validateToken(accessToken)) {   // access token이 만료되지 않았을 경우
+                    Authentication auth = jwtTokenProvider.getAuthentication(accessToken);
+                    SecurityContextHolder.getContext().setAuthentication(auth);
+                }
+            } catch (ExpiredJwtException e) {   // validateToken의 claims.getBody().getExpiration()에서 발생
+                System.out.println("Token expired");
+                throw new RefreshRequiredException();
+            } catch (SignatureException e) {
+                throw new SignFailedException();
+            } catch (MalformedJwtException e) {
+                throw new MalformedJwtException("Provided JWT token's format is not correct.");
+            }
+        }
+        else {  // 로그아웃 혹은 회원탈퇴한 유저
+            System.out.println("Sign-outed or deleted user");
+            throw new BlackListTokenException();
+        }
+
+        filterChain.doFilter(request, response);
     }
 }
